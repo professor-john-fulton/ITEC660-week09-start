d207b04989e326cccbb1efffd2c67127d8ecaa3b
John Fulton <john.fulton2@franklin.edu>
2025-04-15 08:56:33 -0400

    initial commit

---
+++ contact/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .forms import ContactForm
+from .models import ContactMessage
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            ContactMessage.objects.create(**form.cleaned_data)
+            # Process the data (e.g., save to a database or send an email)
+            cleaned_data = form.cleaned_data
+            print(cleaned_data)  # Example processing
+            return render(request, 'contact/thank_you.html')  # Redirect to a thank-you page
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/contact.html', {'form': form})
+
+def home_view(request):
+    return render(request, 'contact/home.html') 
+
